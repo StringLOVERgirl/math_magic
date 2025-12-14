9b34b46d0381754a57755a51db3e2b4796d0ac9d
StringLOVERgirl <marchenkostar@gmail.com>
2025-12-15 00:53:35 +0300

    tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function Canvas() {
        }
 
         setSize=()=>{
-          window.innerWidth < 1250 ? amp = 1280 / window.innerWidth * 0.4 : amp = 1
+          window.innerWidth < 1250 ? amp = 1280 / window.innerWidth * 0.3 : amp = 1
           window.innerWidth < 1250 ? speed = 0.005 : speed = 0.05
         let width = window.innerWidth
         let height = window.innerHeight
