d35a8eb902154d0e0cfee721f60040f6f5afc735
StringLOVERgirl <marchenkostar@gmail.com>
2025-12-15 11:03:24 +0300

    speed mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react'
 import './App.css'
 import img from './bardiel.18.video-poster.jpeg'
 
@@ -9,11 +9,30 @@ function Canvas() {
   
   const bardiel = useRef(new Image())
 
-  function resize(){
+  let amp: number
+  let speed: number
+  let scale:number
 
-  }
+  const setSize = useCallback(():void=>{
+    window.innerWidth < 1250 ? amp = 1280 / window.innerWidth * 0.35 : amp = 1.25
+    window.innerWidth < 1250 ? speed = window.innerWidth / 1280 * 4 * 0.05 : speed = 0.04//3 или 4 или 5  вмето 2 
+    let width = window.innerWidth
+    let height = window.innerHeight
 
-  useLayoutEffect(()=>{
+    let imgwidth = bardiel.current.naturalWidth
+    let imgheight = bardiel.current.naturalHeight
+
+    scale = height/imgheight
+    scale = Number(scale.toFixed(2))
+    console.log(scale)
+
+    if (canvas.current){
+      canvas.current.height = height
+      canvas.current.width = bardiel.current.naturalWidth*scale
+    }
+  },[])
+
+  useEffect(()=>{
 
     if (canvas.current){
        context.current=canvas.current.getContext('2d')
@@ -22,45 +41,27 @@ function Canvas() {
 
     let vawe = 5
     let time = 0.05
-    let amp: number
-    let speed: number
+    
+    let period: number = 5
+    // than smaller - than more frequence
+    
     let bardiel2:HTMLImageElement, context2:CanvasRenderingContext2D, canvas2:HTMLCanvasElement
-    let scale:number
-    let setSize:any
-    let raf: any
+    
+    let raf: number
 
     if(context.current && bardiel.current && canvas.current){
-       bardiel.current.src = img
-       bardiel2 = bardiel.current
-       context2 = context.current
-       canvas2 = canvas.current
-       bardiel2.onload = () => {
-         math_magic()
-       }
-
-        setSize=()=>{
-          window.innerWidth < 1250 ? amp = 1280 / window.innerWidth * 0.35 : amp = 1.25
-          window.innerWidth < 1250 ? speed = window.innerWidth / 1280 * 2 * 0.05 : speed = 0.04
-        let width = window.innerWidth
-        let height = window.innerHeight
-  
-        let imgwidth = bardiel.current.naturalWidth
-        let imgheight = bardiel.current.naturalHeight
-  
-        scale = height/imgheight
-        scale = Number(scale.toFixed(2))
-        console.log(scale)
-        if (canvas.current){
-          canvas.current.height = height
-          canvas.current.width = bardiel.current.naturalWidth*scale
+        bardiel.current.src = img
+        bardiel2 = bardiel.current
+        context2 = context.current
+        canvas2 = canvas.current
+        bardiel.current.onload = () => {
+          math_magic()
         }
-       }
-       window.addEventListener('resize', setSize)
+      
+        window.addEventListener('resize', setSize)
 
     function math_magic(){
-    
-
-     setSize()
+      setSize()
       animate()
     }
 
@@ -68,8 +69,9 @@ function Canvas() {
       context2.clearRect(0,0,canvas2.width, canvas2.height)
       context2.drawImage(bardiel2,0,0, canvas2.width, canvas2.height)
       for(let y=canvas2.height/2; y < canvas2.height; y+=vawe ){
-        let offset = Math.sin(3.14+y/5+time)*amp
-        context2.drawImage(bardiel2, 0, y/scale, canvas2.width/scale, vawe/scale,
+        let offset = Math.sin(3.14+y/period+time)*amp
+        context2.drawImage(bardiel2, 
+         0, y/scale, canvas2.width/scale, vawe/scale,
          offset, y, canvas2.width, vawe )
       }
       time+=speed
@@ -79,7 +81,7 @@ function Canvas() {
     }
 
     return()=> {
-      window.removeEventListener('resize', setSize)
+       window.removeEventListener('resize', setSize)
        cancelAnimationFrame(raf)
     }
   },[])
